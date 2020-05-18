ea31f18cf1f750a2945828873efc4a706d1113aa
crespo742 <57002905+crespo742@users.noreply.github.com>
2020-05-18 13:15:51 +0200

    Update data.php

--- data.php
+++ data.php
@@ -1,8 +1,8 @@
 <?php
 
-$bdd = new PDO('mysql:host=localhost;dbname=cv;charset=utf8', 'root', '');
+$bdd = new PDO("mysql:host=localhost;dbname=cv;charset=utf8", "root", "");
 
-$test= $bdd->query(" INSERT INTO nom_de_la_table");
+$test= $bdd->query (" INSERT INTO nom_de_la_table");
 while ($nom_de_la_table= $test->fetch(PDO::FETCH_OBJ)) {}
 
 echo $test;
